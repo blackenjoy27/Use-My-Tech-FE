61cb330e56b97be9a27e6817fe3052c281406c23
blackenjoy27 <lixunxin@gmail.com>
2021-09-12 13:37:31 -0400

    Created routes for login and sign up components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Homepage from "./components/homepage/Homepage";
+import Signup from "./components/signup-page/Signup";
+import Login from "./components/login/Login";
 
 function App() {
   return (
     <Router>
       <Route path="/">
         <Homepage />
+      </Route>
+      <Route exact path="register">
+        <Signup />
+      </Route>
+      <Route exact path="login">
+        <Login />
       </Route>
     </Router>
   );
